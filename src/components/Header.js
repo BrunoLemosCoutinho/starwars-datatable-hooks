5ee06eb13aeda20dc0d842b4c57bc595d0e2a016
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-06-01 10:31:43 -0300

    style: add author links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,33 @@ import './Header.css';
 
 function Header() {
   return (
-    <div className="header-container">
-      <img src={ r2d2 } alt="R2d2 Robot from StarWars series" />
-      <h1>Star Wars Data Table</h1>
-      <p>Trybe Project</p>
-    </div>
+    <header>
+      <div className="banner">
+        <img src={ r2d2 } alt="R2d2 Robot from StarWars series" />
+        <h1>Star Wars Data Table</h1>
+        <p>Bruno Lemos Coutinho</p>
+      </div>
+      <div className="author_info">
+        <a
+          href="https://www.linkedin.com/in/brunolemoscoutinho/"
+          target="_blank"
+        >
+          Linkedin
+        </a>
+        <a
+          href="https://github.com/BrunoLemosCoutinho"
+          target="_blank"
+        >
+          Github
+        </a>
+        <a
+          href="mailto:brunolemos.ssa@gmail.com"
+          target="_blank"
+        >
+          Email
+        </a>
+      </div>
+    </header>
   );
 }
 
