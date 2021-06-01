9ff78d14ee2a8f7da4da4399cb0defa714aaf9c0
Bruno Lemos <brunolemos.ssa@gmail.com>
2021-06-01 07:03:17 -0300

    Updates

---
+++ precache-manifest.fa300dbc0717190e7648aa3adfca6abd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726d7fa4908e9b7e7797bf9b8175fe95",
+    "url": "/starwars-datatable-hooks/index.html"
+  },
+  {
+    "revision": "61401e1c1b599ab60733",
+    "url": "/starwars-datatable-hooks/static/css/main.a61ea058.chunk.css"
+  },
+  {
+    "revision": "6e351421e5d7f833654f",
+    "url": "/starwars-datatable-hooks/static/js/2.d4b8211a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-datatable-hooks/static/js/2.d4b8211a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61401e1c1b599ab60733",
+    "url": "/starwars-datatable-hooks/static/js/main.d1800b3c.chunk.js"
+  },
+  {
+    "revision": "0078f54fea95fa1ab8c2",
+    "url": "/starwars-datatable-hooks/static/js/runtime-main.5be44aa5.js"
+  },
+  {
+    "revision": "b7d7167592f868ac7cef99c6496bd083",
+    "url": "/starwars-datatable-hooks/static/media/rd2d.b7d71675.png"
+  }
+]);
